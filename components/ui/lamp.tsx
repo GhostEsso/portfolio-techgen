eb7070a000d49321319f1e5f443f2652192dedee
GhostEsso <lilipitaham@gmail.com>
2024-06-25 15:25:17 +0000

    navbar issues fixed

--- components/ui/lamp.tsx
+++ components/ui/lamp.tsx
@@ -5,42 +5,61 @@ import { cn } from "@/utils/cn";
 
 export default function LampDemo() {
   return (
-    <LampContainer>
+    <>
       <Navbar />
-      <motion.h1
-        initial={{ opacity: 0.5, y: 100 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-        className="mt-24 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-5xl font-medium tracking-tight text-transparent md:text-7xl"
-      >
-        Optimize your business <br /> growth with vetted developers.
-      </motion.h1>
-    </LampContainer>
+      <LampContainer>
+        <motion.h1
+          initial={{ opacity: 0.5, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="mt-24 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-3xl font-medium tracking-tight text-transparent md:text-7xl"
+        >
+          Optimize your business <br /> growth with vetted developers.
+          <p className="text-xl">
+            <br />
+            With unlimited design projects and 100+ design services, we&apos;ve
+            got your back.
+          </p>
+        </motion.h1>
+      </LampContainer>
+    </>
   );
 }
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
-  // Fonction pour fermer le menu
   const closeMenu = () => setMenuOpen(false);
 
   return (
-    <nav className="fixed top-0 left-0 w-full bg-transparent text-white flex justify-between items-center px-6 py-4 z-50 pt-6">
+    <nav className="fixed top-0 left-0 w-full bg-transparent text-white flex justify-between items-center px-6 py-4 z-50 pt-2">
       <NexGenLogo />
       <div className="hidden md:flex items-center space-x-4">
-        <a href="#why-nexgen" className="hover:underline">Why TechGen?</a>
-        <a href="#how-it-works" className="hover:underline">How it works</a>
-        <a href="#pricing" className="hover:underline">Pricing</a>
-        <a href="#our-work" className="hover:underline">Our work</a>
-        <button className="bg-cyan-500 text-white py-2 px-4 rounded hover:bg-cyan-600">Book Call</button>
+        <a href="#why-nexgen" className="hover:underline">
+          Why TechGen?
+        </a>
+        <a href="#how-it-works" className="hover:underline">
+          How it works
+        </a>
+        <a href="#pricing" className="hover:underline">
+          Pricing
+        </a>
+        <a href="#our-work" className="hover:underline">
+          Our work
+        </a>
+        <button className="bg-cyan-500 text-white py-2 px-4 rounded hover:bg-cyan-600">
+          Book Call
+        </button>
       </div>
       <div className="md:hidden">
-        <button onClick={() => setMenuOpen(!menuOpen)} className="focus:outline-none">
+        <button
+          onClick={() => setMenuOpen(!menuOpen)}
+          className="focus:outline-none"
+        >
           <svg
             className="w-6 h-6 text-white"
             fill="none"
@@ -48,17 +67,51 @@ const Navbar = () => {
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16m-7 6h7"
+            />
           </svg>
         </button>
       </div>
       {menuOpen && (
-        <div className="absolute top-16 left-0 w-full text-black flex flex-col items-center space-y-4 py-4 md:hidden pt-10 rounded-xl bg-white transition">
-          <a href="#why-nexgen" className="hover:underline text-xl" onClick={closeMenu}>Why TechGen?</a>
-          <a href="#how-it-works" className="hover:underline text-xl" onClick={closeMenu}>How it works</a>
-          <a href="#pricing" className="hover:underline text-xl" onClick={closeMenu}>Pricing</a>
-          <a href="#our-work" className="hover:underline text-xl" onClick={closeMenu}>Our work</a>
-          <button className="bg-cyan-500 text-white py-2 px-32 rounded-md hover:bg-cyan-600 text-xl" onClick={closeMenu}>Book Call</button>
+        <div className="absolute top-12 left-0 w-full text-black flex flex-col items-center space-y-4 py-4 md:hidden bg-white rounded-xl transition">
+          <a
+            href="#why-nexgen"
+            className="hover:underline text-xl"
+            onClick={closeMenu}
+          >
+            Why TechGen?
+          </a>
+          <a
+            href="#how-it-works"
+            className="hover:underline text-xl"
+            onClick={closeMenu}
+          >
+            How it works
+          </a>
+          <a
+            href="#pricing"
+            className="hover:underline text-xl"
+            onClick={closeMenu}
+          >
+            Pricing
+          </a>
+          <a
+            href="#our-work"
+            className="hover:underline text-xl"
+            onClick={closeMenu}
+          >
+            Our work
+          </a>
+          <button
+            className="bg-cyan-500 text-white py-2 px-4 rounded hover:bg-cyan-600 text-xl"
+            onClick={closeMenu}
+          >
+            Book Call
+          </button>
         </div>
       )}
     </nav>
@@ -66,8 +119,22 @@ const Navbar = () => {
 };
 
 const NexGenLogo = () => (
-  <svg width="90" height="40" viewBox="0 0 100 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <text x="0" y="20" fill="#0EB2CE" fontSize="25" fontFamily="Arial, sans-serif">TechGen</text>
+  <svg
+    width="90"
+    height="40"
+    viewBox="0 0 100 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <text
+      x="0"
+      y="20"
+      fill="#0EB2CE"
+      fontSize="25"
+      fontFamily="Arial, sans-serif"
+    >
+      TechGen
+    </text>
   </svg>
 );
 
@@ -99,8 +166,8 @@ export const LampContainer = ({
           }}
           className="absolute inset-auto right-1/2 h-56 overflow-visible w-[30rem] bg-gradient-conic from-cyan-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]"
         >
-          <div className="absolute  w-[100%] left-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
-          <div className="absolute  w-40 h-[100%] left-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_right,white,transparent)]" />
+          <div className="absolute w-[100%] left-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
+          <div className="absolute w-40 h-[100%] left-0 bg-slate-950 bottom-0 z-20 [mask-image:linear-gradient(to_right,white,transparent)]" />
         </motion.div>
         <motion.div
           initial={{ opacity: 0.5, width: "15rem" }}
@@ -115,8 +182,8 @@ export const LampContainer = ({
           }}
           className="absolute inset-auto left-1/2 h-56 w-[30rem] bg-gradient-conic from-transparent via-transparent to-cyan-500 text-white [--conic-position:from_290deg_at_center_top]"
         >
-          <div className="absolute  w-40 h-[100%] right-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
-          <div className="absolute  w-[100%] right-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
+          <div className="absolute w-40 h-[100%] right-0 bg-slate-950 bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
+          <div className="absolute w-[100%] right-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
         </motion.div>
         <div className="absolute top-1/2 h-48 w-full translate-y-12 scale-x-150 bg-slate-950 blur-2xl"></div>
         <div className="absolute top-1/2 z-50 h-48 w-full bg-transparent opacity-10 backdrop-blur-md"></div>
